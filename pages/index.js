5bbd24d7655a9a8888228fdc6dedd710ddcc69fa
Eduardo Pereira <133928331+edu99p@users.noreply.github.com>
2024-07-07 18:11:19 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
-function Home()
-{
-    return <h1><a href="https://www.youtube.com/watch?v=r4vKDro9YRg"
-    target="_blank">Ventos da mudança!</a></h1>
-
+function Home() {
+  return (
+    <h1>
+      <a href="https://www.youtube.com/watch?v=r4vKDro9YRg" target="_blank">
+        Ventos da mudança!
+      </a>
+    </h1>
+  );
 }
 
 export default Home;
